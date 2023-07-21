14014d3474601a28d158c2fe0b8f484428e1a2bd
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 15:17:04 +0200

    created new migration to modify 'departure_time' and 'arrival_time' type from 'time' to 'datetime' in table 'trains'

---
+++ database/migrations/2023_07_21_130849_change_departure_arrival_type_to_trains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->datetime('departure_time')->change();
+            $table->datetime('arrival_time')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('departure_time')->change();
+            $table->time('arrival_time')->change();
+        });
+    }
+};
