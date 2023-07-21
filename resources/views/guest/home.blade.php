69bd8cebd0e7798ec3358d9a551a9f514bba5c01
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 15:51:03 +0200

    added foreach to homepage that prints every train info from $trains variable

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,5 +3,16 @@
 @section('title', 'Homepage')
 
 @section('main-section')
-    <h1>Welcome to the Homepage!</h1>
+    @foreach($trains as $train)
+        <p><span>Id: </span>{{ $train->id }}</p>
+        <p><span>Agency: </span>{{ $train->agency }}</p>
+        <p><span>Departure Station: </span>{{ $train->departure_station }}</p>
+        <p><span>Arrival Station: </span>{{ $train->arrival_station }}</p>
+        <p><span>Departure Time: </span>{{ $train->departure_time }}</p>
+        <p><span>Arrival Time: </span>{{ $train->arrival_time }}</p>
+        <p><span>Train Code: </span>{{ $train->train_code }}</p>
+        <p><span>Carriages Number: </span>{{ $train->carriages_number }}</p>
+        <p><span>On Time: </span>{{ $train->isOnTime ? 'Yes' : 'No' }}</p>
+        <p><span>Cancelled: </span>{{ $train->isCancelled ? 'Yes' : 'No' }}</p>
+    @endforeach
 @endsection
