f2a5d916a3a56fe8dfa14f9e208242f3ae294f71
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 15:42:10 +0200

    imported Train Model, added function home that takes table trains records, assign them to variable $trains and passes it to home view with compact

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home() {
+        $trains = Train::all();
+        return view('guest.home', compact('trains'));
+    }
 }
