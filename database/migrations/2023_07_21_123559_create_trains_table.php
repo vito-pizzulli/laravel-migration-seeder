fc3a23e295ca9f966c1e9889c8a8dd3c785bdd6e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 14:56:21 +0200

    Milestone 1 completed - created trains table with its migration

---
+++ database/migrations/2023_07_21_123559_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->smallInteger('train_code')->unsigned();
+            $table->tinyInteger('carriages_number')->unsigned();
+            $table->boolean('isOnTime');
+            $table->boolean('isCancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
