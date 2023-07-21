bc3c11bb2a6ccca75eb0d021a08d2e54757068a1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 16:31:05 +0200

    created Seeder Train

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
