ccf5a3bc4a850c67878dc2a6721248f8ba96040a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-21 17:36:39 +0200

    added background gif, scss & bootstrap rules, styling completed

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,16 +3,31 @@
 @section('title', 'Homepage')
 
 @section('main-section')
-    @foreach($trains as $train)
-        <p><span>Id: </span>{{ $train->id }}</p>
-        <p><span>Agency: </span>{{ $train->agency }}</p>
-        <p><span>Departure Station: </span>{{ $train->departure_station }}</p>
-        <p><span>Arrival Station: </span>{{ $train->arrival_station }}</p>
-        <p><span>Departure Time: </span>{{ $train->departure_time }}</p>
-        <p><span>Arrival Time: </span>{{ $train->arrival_time }}</p>
-        <p><span>Train Code: </span>{{ $train->train_code }}</p>
-        <p><span>Carriages Number: </span>{{ $train->carriages_number }}</p>
-        <p><span>On Time: </span>{{ $train->isOnTime ? 'Yes' : 'No' }}</p>
-        <p><span>Cancelled: </span>{{ $train->isCancelled ? 'Yes' : 'No' }}</p>
-    @endforeach
+    <div class="card-container d-flex justify-content-center flex-wrap">
+        @foreach($trains as $train)
+            <div class="card m-3" style="width: calc(100% / 3 - 2rem)">
+                <div class="card-body text-center">
+                    <p><span>Id: </span>{{ $train->id }}</p>
+                    <hr>
+                    <p><span>Agency: </span>{{ $train->agency }}</p>
+                    <hr>
+                    <p><span>Departure Station: </span>{{ $train->departure_station }}</p>
+                    <hr>
+                    <p><span>Arrival Station: </span>{{ $train->arrival_station }}</p>
+                    <hr>
+                    <p><span>Departure Time: </span>{{ $train->departure_time }}</p>
+                    <hr>
+                    <p><span>Arrival Time: </span>{{ $train->arrival_time }}</p>
+                    <hr>
+                    <p><span>Train Code: </span>{{ $train->train_code }}</p>
+                    <hr>
+                    <p><span>Carriages Number: </span>{{ $train->carriages_number }}</p>
+                    <hr>
+                    <p><span>On Time: </span>{{ $train->isOnTime ? 'Yes' : 'No' }}</p>
+                    <hr>
+                    <p><span>Cancelled: </span>{{ $train->isCancelled ? 'Yes' : 'No' }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header>
-    <h1>Laravel Migration Seeder</h1>
-    <h2>Trains</h2>
+<header class="d-flex flex-column justify-content-center">
+    <h1 class="text-center text-uppercase">Laravel Migration Seeder</h1>
+    <h2 class="text-center text-white">Trains</h2>
 </header>
